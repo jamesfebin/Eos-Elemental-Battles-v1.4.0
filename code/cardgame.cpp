2ab9c41357ad3802d11c1580c1ec4c78094f9d7a
Shubhorup Biswas <shubhorup.biswas@dunyalabs.io>
2018-11-15 10:37:42 +0530

    Lesson 2 in v1.4.0

--- code/cardgame.cpp
+++ code/cardgame.cpp
@@ -1,3 +1,3 @@
 #include "gameplay.cpp"
 
-EOSIO_DISPATCH(cardgame, BOOST_PP_SEQ_NIL)
+EOSIO_DISPATCH(cardgame, (login))
--- code/cardgame.hpp
+++ code/cardgame.hpp
@@ -3,8 +3,26 @@
 using namespace eosio;
 CONTRACT cardgame : public contract {
 
+  private:
+
+    TABLE user_info {
+      name    name;
+      uint16_t        win_count = 0;
+      uint16_t        lost_count = 0;
+
+      auto primary_key() const { return name.value; }
+    };
+
+    typedef multi_index<"users"_n, user_info> users_table;
+
+    users_table _users;
+
+
+
   public:
 
-    cardgame( name receiver, name code, datastream<const char*> ds):contract(receiver,code, ds ){}
+    cardgame( name receiver, name code, datastream<const char*> ds):contract(receiver,code, ds),_users(code, code.value){}
+
+    ACTION login(name username);
 
 };
